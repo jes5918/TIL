76101be18c92ba7d7ab8a61aa36c0ed1499fcccd
jes5918 <jes5918@koreatech.ac.kr>
2020-08-10 18:54:23 +0900

    0810 commit

--- Algorithm/SWEA/D2/D2_1954_달팽이숫자.py
+++ Algorithm/SWEA/D2/D2_1954_달팽이숫자.py
@@ -61,37 +61,38 @@
 #         cnt -= 1
 #     print(dalpang)
 
-cnt=1
-arr=[[0]*5 for i in range(5)]
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    cnt = 1
+    dalpang = [[0 for j in range(N)] for i in range(N)] # 배열 0으로 초기화
 
-row_start=0
-row_end=4
-col_start=0
-col_end=4
+    r_st = 0
+    r_end = N-1
+    c_st = 0
+    c_end = N-1
 
-while row_start<=row_end and col_start<=col_end:
-    # 왼쪽=> 오른쪽
-    for i in range(col_start, col_end+1):
-        arr[row_start][i]=cnt
-        cnt+=1
-    row_start +=1
+    while c_st <= c_end and r_st <= r_end:
+        for i in range(c_st, c_end+1):  # 왼쪽에서 오른쪽
+            dalpang[r_st][i] = cnt
+            cnt += 1
+        r_st += 1
 
-    # 위=> 아래
-    for i in range(row_start, row_end + 1):
-        arr[i][col_end] = cnt
-        cnt += 1
-    col_end -= 1
+        for i in range(r_st, r_end + 1): # 위에서 아래
+            dalpang[i][c_end] = cnt
+            cnt += 1
+        c_end -= 1
 
-    # 오른쪽=> 왼쪽
-    for i in range(col_end, col_start-1, -1):
-        arr[row_end][i]=cnt
-        cnt+=1
-    row_end -=1
+        for i in range(c_end, c_st-1, -1): # 오른쪽에서 왼쪽
+            dalpang[r_end][i] = cnt
+            cnt += 1
+        r_end -= 1
 
-    # 아래=> 위
-    for i in range(row_end, row_start - 1, -1):
-        arr[i][col_start] = cnt
-        cnt += 1
-    col_start += 1
+        for i in range(r_end, r_st - 1, -1): # 아래에서 위쪽
+            dalpang[i][c_st] = cnt
+            cnt += 1
+        c_st += 1
 
-print(arr)
+    print(f'#{tc}')
+    for i in range(N):
+        print(*dalpang[i]) # 아주 좋아 이 표현식
--- Algorithm/SWEA/D2/test.py
+++ Algorithm/SWEA/D2/test.py
@@ -1,8 +1,22 @@
-a = 1
-def f1():
-    a = 5
-    def f2():
-        print(a, end='')
-    f2()
-f1()
-print(a)
+for t in range(int(input())):
+    n=int(input())
+    b=[[0]*n for i in range(n)]
+    dy=[0,1,0,-1]
+    dx=[1,0,-1,0]
+    d=r=c=0
+    l=1
+    while(l<=n*n):
+        b[r][c]=l
+        l+=1
+        nr=r+dy[d]
+        nc=c+dx[d]
+        if 0<=nr<n and 0<=nc<n and b[nr][nc]==0:
+            r,c=nr,nc
+        else:
+            d=(d+1)%4
+            r+=dy[d]
+            c+=dx[d]
+    print(f'#{t+1}')
+    for i in range(n):
+        for j in range(n):print(b[i][j],end=' ')
+        print()
