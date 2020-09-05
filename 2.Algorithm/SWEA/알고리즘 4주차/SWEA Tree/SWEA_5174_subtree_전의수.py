a3b2813e0654deec9d9b6b58ea473426999cfa56
Euisunotebook <wjsdmltntn@gmail.com>
2020-09-05 18:47:17 +0900

    0905

--- 2.Algorithm/SWEA/알고리즘 4주차/SWEA Tree/SWEA_5174_subtree_전의수.py
+++ 2.Algorithm/SWEA/알고리즘 4주차/SWEA Tree/SWEA_5174_subtree_전의수.py
@@ -1,3 +1,10 @@
 import sys
 sys.stdin = open('5174.txt', 'r')
 
+for tc in range(1, int(input())+1):
+    E, N = map(int, input().split())
+    lines = list(map(int, input().split()))
+    visited = []
+    for i in range(E):
+        lines[i*2]
+        lines[i*2+1]
